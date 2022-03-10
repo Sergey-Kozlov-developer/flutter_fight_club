c11906b427ffa4cecd55319bebe0958fc5a4de39
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2022-03-10 17:26:29 +0300

    new

--- lib/resources/fight_club_colors.dart
+++ lib/resources/fight_club_colors.dart
@@ -11,6 +11,7 @@ class FightClubColors {
   static const blackButton = Color.fromRGBO(0, 0, 0, 0.87);
   static const darkGreyText = Color.fromRGBO(22, 22, 22, 1);
   static const whiteText = Color.fromRGBO(255, 255, 255, 0.87);
+  static const darkPurple = Color.fromRGBO(197, 209, 234, 1);
 
 
 }
---
+++ lib/widgets/secondary_action_button.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SecondaryActionButton extends StatelessWidget {
+  const SecondaryActionButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
